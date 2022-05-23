a92ec05f6cf570c8c67e48b18f4b1bf9038266d3
Pierre-Philippe Emond <emondpph@gmail.com>
2022-05-23 10:08:48 -0400

    bump version

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -3,7 +3,7 @@ require('laravel-mix-polyfill');
 
 mix.js('index.js', 'dist/navigation.js');
 mix.js('test/navigation.js', 'dist/navigation-test.js');
-mix.polyfill({ enabled: true, useBuiltIns: false });
+mix.alias({ '@': __dirname });
 
 mix.webpackConfig({
     output: {
@@ -11,3 +11,8 @@ mix.webpackConfig({
         libraryTarget: "window"
     }
 });
+
+if(mix.inProduction())
+mix.polyfill({ enabled: true, useBuiltIns: false });
+else
+mix.sourceMaps();
